513d7292578eb91a71fd80e1cd5f5c95a207d0f8
AtinAgnihotri <atinagnihotri@gmail.com>
2021-06-27 22:06:51 +0530

    Adds ExpenseListItem view

---
+++ ExpenseTrackingApp/Views/ExpenseListItem.swift
@@ -0,0 +1,40 @@
+//
+//  ExpenseListItem.swift
+//  ExpenseTrackingApp
+//
+//  Created by Atin Agnihotri on 27/06/21.
+//
+
+import SwiftUI
+
+struct ExpenseListItem: View {
+    var currecySymbol: String = "$"
+    var expenseItem: ExpenseItem
+    
+    init(_ expenseItem: ExpenseItem) {
+        self.expenseItem = expenseItem
+    }
+    
+    var body: some View {
+        HStack {
+            VStack (alignment: .leading) {
+                Text(expenseItem.name)
+                    .font(.headline)
+                    .padding(.horizontal)
+                Text(expenseItem.type)
+                    .font(.subheadline)
+                    .padding(.horizontal)
+            }
+            Spacer()
+            Text("\(currecySymbol)\(expenseItem.amount, specifier: "%g")")
+                .font(.title)
+                .padding()
+        }
+    }
+}
+
+struct ExpenseDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpenseListItem( ExpenseItem(name: "Test Expense", amount: 20, type: "Business"))
+    }
+}
