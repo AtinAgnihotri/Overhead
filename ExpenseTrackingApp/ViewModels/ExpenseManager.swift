56868802b7355383a6754800f5ecb88e9c6c7afe
AtinAgnihotri <atinagnihotri@gmail.com>
2021-07-01 09:10:08 +0530

    Adds local persistence through UserDefaults

--- ExpenseTrackingApp/ViewModels/ExpenseManager.swift
+++ ExpenseTrackingApp/ViewModels/ExpenseManager.swift
@@ -10,9 +10,25 @@ import Foundation
 class ExpenseManager: ObservableObject {
     static private var shared = ExpenseManager()
     
-    @Published var expenseList = [ExpenseItem]() 
+    @Published var expenseList = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let data = try? encoder.encode(expenseList) {
+                UserDefaults.standard.set(data, forKey: "ExpenseItems")
+            }
+        }
+    }
     
-    private init() { }
+    private init() {
+        if let expenseData = UserDefaults.standard.data(forKey: "ExpenseItems") {
+            let decoder = JSONDecoder()
+            if let expenseItems = try? decoder.decode([ExpenseItem].self, from: expenseData ) {
+                self.expenseList = expenseItems
+                return
+            }
+        }
+        self.expenseList = [ExpenseItem]()
+    }
     
     static func getInstance() -> ExpenseManager {
         return shared
